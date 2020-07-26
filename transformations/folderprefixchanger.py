0bd880618d96442678d2efd47cb3baaf2ab6f9fd
Mickaël Bucas <mbucas@gmail.com>
2020-07-26 19:03:30 +0200

    Implement FolderPrefixChanger transformation

--- transformations/folderprefixchanger.py
+++ transformations/folderprefixchanger.py
@@ -21,23 +21,43 @@ class FolderPrefixChanger(AbstractTransformation):
             'values': ['Remove Prefix', 'Change Prefix', 'Add Prefix', ],
         })
         self.expectedProperties.append({
-            'key': 'folderValue',
-            'label': 'Folder Value',
+            'key': 'folderRemoveValue',
+            'label': 'Folder Prefix To Remove',
             'content': 'string',
             'desc': 'Folder prefix to remove',
         })
         self.expectedProperties.append({
-            'key': 'pattern',
-            'label': 'Pattern',
+            'key': 'folderAddValue',
+            'label': 'Folder Prefix To Add',
             'content': 'string',
             'desc': 'Folder prefix to add',
         })
 
+    def prepare_run(self):
+        pass
+
     def transform(self, mailFolder, mail):
-        if mailFolder.startswith(self.properties["folderValue"]):
-            return None, None
+        if self.properties["changeMethod"] == 'Remove Prefix':
+            if mailFolder.startswith(self.properties["folderRemoveValue"]):
+                newMailFolder = mailFolder.replace(
+                    self.properties["folderRemoveValue"],
+                    '',
+                    1  # Only the Prefix
+                )
+                return mailFolder, mail
+        elif self.properties["changeMethod"] == 'Add Prefix':
+            newMailFolder = self.properties["folderAddValue"] + mailFolder
+            return newMailFolder, mail
+        elif self.properties["changeMethod"] == 'Change Prefix':
+            if mailFolder.startswith(self.properties["folderRemoveValue"]):
+                newMailFolder = mailFolder.replace(
+                    self.properties["folderRemoveValue"],
+                    self.properties["folderAddValue"],
+                    1  # Only the Prefix
+                )
+                return newMailFolder, mail
         else:
-            return mailFolder, mail
+            raise Exception("Transformation Description is invalid")
 
 
 RegisterTransformation(FolderPrefixChanger)
